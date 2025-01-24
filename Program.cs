2119741a2f0083dd4dea92f5490b1d5750786955
KyrylOlshevskyi <ipz233_oks@studen.ztu.edu.ua>
2025-01-25 00:37:10 +0200

    Додано проект для обчислення суми двох чисел

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введіть перше число: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Введіть друге число: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        int sum = num1 + num2;
+        Console.WriteLine($"Сума двох чисел: {sum}");
+    }
+}
